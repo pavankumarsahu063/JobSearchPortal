cf69f0681fba6f93fbe8814644128e4f2c649362
Pavan Kumar Sahu <141017150+pavankumarsahu063@users.noreply.github.com>
2025-05-01 23:44:58 +0530

    Update Achievents.java

--- src/main/java/com/jobsearch/model/Achievents.java
+++ src/main/java/com/jobsearch/model/Achievents.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+
 @Entity
 @Table(name = "achievements")
 public class Achievents {
